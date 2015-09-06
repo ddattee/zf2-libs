028477e78842f495b51870d0cc8ee2eed139685e
David Dattee <david.dattee@gmail.com>
2015-09-06 14:18:59 +0200

    General - Doctrine - nettoyage de l'entity

--- library/General/Doctrine/Entity.php
+++ library/General/Doctrine/Entity.php
@@ -8,24 +8,8 @@
 
 namespace General\Doctrine;
 
-use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorAwareTrait;
 
-class Entity implements ServiceLocatorAwareInterface
+abstract class Entity
 {
-	use ServiceLocatorAwareTrait;
 
-	public function getArrayCopy()
-	{
-		return get_object_vars($this);
-	}
-
-	public function exchangeArray($data)
-	{
-		foreach ($data as $k => $val) {
-			$setter = 'set' . ucfirst($k);
-			if (method_exists($this, $setter))
-				$this->$setter($val);
-		}
-	}
 }
