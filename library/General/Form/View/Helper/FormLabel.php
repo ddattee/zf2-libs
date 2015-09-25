408e58a8f6d15750f6e626d4c04fd5aae6a5c7b3
David Dattee <david@subvitamine.com>
2015-09-25 16:21:02 +0200

    General - Form - ajout des helper de vue

---
+++ library/General/Form/View/Helper/FormLabel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ddattee
+ * Date: 03/07/2015
+ * Time: 11:27
+ */
+
+namespace General\Form\View\Helper;
+
+use TwbBundle\View\Helper\TwbBundleLabel;
+use Zend\Form\ElementInterface;
+
+class FormLabel
+{
+	/**
+	 * Invokable
+	 *
+	 * @return str
+	 */
+//	public function __invoke(ElementInterface $element = null, $labelContent = null, $position = null)
+//	{
+//		return ($element->hasAttribute('required') ? '<span class="required-mark">*</span>' : '') . parent::__invoke($element, $labelContent, $position);
+//	}
+
+}
