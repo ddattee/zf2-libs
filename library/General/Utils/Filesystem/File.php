6b2405a9ba060e32d746d091315c43addfd9c4b3
David Dattee <david.dattee@gmail.com>
2015-10-28 14:20:06 +0100

    File - ajout de la fonction pour formater des bit pour affichage

--- library/General/Utils/Filesystem/File.php
+++ library/General/Utils/Filesystem/File.php
@@ -19,5 +19,25 @@ class File
 	{
 		return substr($filename, strrpos($filename, '.'));
 	}
+	
+	/**
+	 * Format Bytes value into the appropriate format (o, Mo, Go, To)
+	 * @param int $bytes
+	 * @param int $precision
+	 * @return string
+	 */
+	public static function formatBytes($bytes, $precision = 2) {
+		$units = array('o', 'Ko', 'Mo', 'Go', 'To');
+
+		$bytes = max($bytes, 0);
+		$pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+		$pow = min($pow, count($units) - 1);
+
+		// Uncomment one of the following alternatives
+		$bytes /= pow(1024, $pow);
+		// $bytes /= (1 << (10 * $pow)); 
+
+		return round($bytes, $precision) . ' ' . $units[$pow];
+	}
 
 }
