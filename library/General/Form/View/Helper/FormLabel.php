a7e7f04de4636ee43eb7ddfdd7187c199e64123c
David Dattee <dadat@smile.fr>
2016-09-01 10:58:56 +0200

    General - Updating form label view helper

--- library/General/Form/View/Helper/FormLabel.php
+++ library/General/Form/View/Helper/FormLabel.php
@@ -13,14 +13,28 @@ use Zend\Form\ElementInterface;
 
 class FormLabel
 {
-	/**
-	 * Invokable
-	 *
-	 * @return str
-	 */
-//	public function __invoke(ElementInterface $element = null, $labelContent = null, $position = null)
-//	{
-//		return ($element->hasAttribute('required') ? '<span class="required-mark">*</span>' : '') . parent::__invoke($element, $labelContent, $position);
-//	}
+    /**
+     * Invokable
+     *
+     * @param ElementInterface|null $element
+     * @param null $labelContent
+     * @param null $position
+     *
+     * @return string
+     */
+    public function __invoke(ElementInterface $element = null, $labelContent = null, $position = null)
+    {
 
+        // invoke parent and get form label
+        $originalformLabel = parent::__invoke($element, $labelContent, $position);
+
+        // check if element is required
+        if ($element->hasAttribute('required')) {
+            // add a start to required elements
+            return '<span class="required-mark">*</span>' . $originalformLabel;
+        } else {
+            // not start to optional elements
+            return  $originalformLabel;
+        }
+    }
 }
