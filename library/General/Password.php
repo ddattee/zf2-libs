d64715a45a4520c2b9d4d6c8386ae9e0d6448720
David Dattee <david.dattee@gmail.com>
2016-10-09 13:28:32 +0200

    Adding Password generator class

---
+++ library/General/Password.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ddattee
+ * Date: 29/06/2015
+ * Time: 15:01
+ */
+
+namespace General;
+
+/**
+ * Class Password
+ *
+ * @package General
+ */
+class Password
+{
+    /**
+     * Generate random password with letters (upper and lower), number and some special char
+     *
+     * @return string
+     */
+    public static function generate() {
+        $seed = str_split('abcdefghijklmnopqrstuvwxyz'
+            . 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+            . '0123456789'
+            . '@!.$€');
+        shuffle($seed);
+        $rand = '';
+        foreach (array_rand($seed, 10) as $k)
+            $rand .= $seed[$k];
+
+        return $rand;
+    }
+}
