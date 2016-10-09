faf393d7497dcd838b34ee45dc8979bb3223ad0e
David Dattee <david.dattee@gmail.com>
2016-10-09 18:28:47 +0200

    Form - Validator - adding + allowed in email syntaxe

--- library/General/Validator/EmailSyntaxe.php
+++ library/General/Validator/EmailSyntaxe.php
@@ -12,22 +12,22 @@ use Zend\Validator\AbstractValidator;
 class EmailSyntaxe extends AbstractValidator
 {
 
-	const INVALID_EMAILSYNT = 'invalidEmailSyntax';
+    const INVALID_EMAILSYNT = 'invalidEmailSyntax';
 
-	protected $messageTemplates = array(
-		self::INVALID_EMAILSYNT => "'%value%' is not a valid email.",
-	);
+    protected $messageTemplates = array(
+        self::INVALID_EMAILSYNT => "'%value%' is not a valid email.",
+    );
 
-	public function isValid($value)
-	{
-		$valueString = strtolower((string)$value);
-		$this->setValue($valueString);
-		$regex = '/^[a-z0-9_.-]+@[a-z0-9.-]+\.[a-z]{2,}$/';
+    public function isValid($value)
+    {
+        $valueString = strtolower((string)$value);
+        $this->setValue($valueString);
+        $regex = '/^[a-z0-9_.+-]+@[a-z0-9.-]+\.[a-z]{2,}$/';
 
-		if (!preg_match($regex, $valueString)) {
-			$this->error(self::INVALID_EMAILSYNT);
-			return false;
-		}
-		return true;
-	}
+        if (!preg_match($regex, $valueString)) {
+            $this->error(self::INVALID_EMAILSYNT);
+            return false;
+        }
+        return true;
+    }
 }
