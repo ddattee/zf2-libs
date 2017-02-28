d9c9928a5231ef4abffd8ed7a0f7c2fc05af484a
David Dattee <david.dattee@gmail.com>
2017-02-28 20:43:01 +0100

    Git - refactoring and adding reset

--- library/General/Utils/Git.php
+++ library/General/Utils/Git.php
@@ -29,6 +29,32 @@ class Git
     }
 
     /**
+     * Call command
+     *
+     * @param string $cmd     Command to call
+     * @param null   $cwd     Path to run the command into
+     * @param string &$output Command output
+     * @param string &$errors Command error return
+     *
+     * @return int|mixed
+     */
+    protected function call($cmd, $cwd = null, &$output = '', &$errors = '') {
+        //Run the command
+        $descriptorspec = array(1 => array('pipe', 'w'), 2 => array('pipe', 'a'));
+        $resource = proc_open($cmd, $descriptorspec, $pipes, $cwd);
+        if (is_resource($resource)) {
+            $output = stream_get_contents($pipes[1]);
+            $errors = stream_get_contents($pipes[2]);
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+            $status = proc_get_status($resource);
+            proc_close($resource);
+        }
+
+        return $status['running'] === false ? $status['exitcode'] : $status['running'];
+    }
+
+    /**
      * Try to pull from current repository
      *
      * @param string $remoteName   Remote central repositoriy name to pull from
@@ -38,7 +64,7 @@ class Git
      * @param string &$errors      Command error return
      * @param null   $cwd          Path to run the command into
      *
-     * @return int
+     * @return int|mixed
      */
     public function pull($remoteName = 'origin', $remoteBranch = 'master', $localBranch = 'master', &$output = '', &$errors = '', $cwd = null)
     {
@@ -53,18 +79,36 @@ class Git
         $cmd = "/usr/bin/git pull";
         //Git pull params
         $cmd .= ' ' . $remoteName . ' ' . $remoteBranch . ':' . $localBranch;
-        //Run the command
-        $descriptorspec = array(1 => array('pipe', 'w'), 2 => array('pipe', 'a'));
-        $resource = proc_open($cmd, $descriptorspec, $pipes, $cwd);
-        if (is_resource($resource)) {
-            $output = stream_get_contents($pipes[1]);
-            $errors = stream_get_contents($pipes[2]);
-            fclose($pipes[1]);
-            fclose($pipes[2]);
-            $status = proc_get_status($resource);
-            proc_close($resource);
-        }
 
-        return $status['running'] === false ? $status['exitcode'] : $status['running'];
+        return $this->call($cmd, $cwd, $output, $errors);
+    }
+
+    /**
+     * Git reset
+     *
+     * @param string $remoteName   Remote central repositoriy name to pull from
+     * @param string $remoteBranch Remote branch to pull from
+     * @param string $localBranch  Local branch to update
+     * @param bool   $hard         Reset hard if not HEAD will be used
+     * @param string &$output      Command output
+     * @param string &$errors      Command error return
+     * @param null   $cwd          Path to run the command into
+     *
+     * @return int|mixed
+     */
+    public function reset($remoteName = 'origin', $remoteBranch = 'master', $localBranch = 'master', $hard = false, &$output = '', &$errors = '', $cwd = null) {
+        //Locate the repository
+        $cwd = !$cwd ? $this->getRepoPath() : $cwd;
+        //Check for git dir
+        if (!is_dir($cwd.'/.git')) {
+            $errors .= "No git repo found.";
+            return -100;
+        }
+        //Set the command to call
+        $cmd = "/usr/bin/git pull";
+        //Git reset params
+        $cmd .= ' ' . ($hard ? '--hard' : 'HEAD') . ' ' . $remoteName . ' ' . $remoteBranch . ':' . $localBranch;
+
+        return $this->call($cmd, $cwd, $output, $errors);
     }
 }
