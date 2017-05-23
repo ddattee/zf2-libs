6e9d2e73ba9d749f387b14ba61ae89489149be43
Admin <david.dattee@gmail.com>
2017-05-23 17:54:07 +0200

    Update docker_install.sh adding composer install

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git -yqq
+apt-get install git composer -yqq
 
 # Install mysql driver
 # Here you can install any other extension that you need
